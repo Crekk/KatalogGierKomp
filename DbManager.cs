12d0eabbe62a919f841c35335d053cc9e84fd569
Arkadiusz Moskal <arrrek6@gmail.com>
2026-04-17 01:26:06 +0200

    Add project files.

---
+++ DbManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.Sqlite;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KatalogGierKomp
+{
+    public class DbManager
+    {
+        private readonly string ConnectionString = "";
+        private readonly string DbPath = "";
+
+        public static void Initialize(string DbPath)
+        {
+            bool dbExists = File.Exists(DbPath);
+
+            if (dbExists)
+            {
+                //later check if tables exist and are correct, if not, create or fix them, not just if the file exists
+                Console.WriteLine("Database exists. Skipping initialization");
+                return;
+            }
+            if (!dbExists)
+            {
+                using (var connection = new SqliteConnection($"Data Source={DbPath}"))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText =
+                    @"
+                    CREATE TABLE firmy (
+                        id_firma INTEGER PRIMARY KEY,
+                        nip TEXT NOT NULL UNIQUE
+                            CHECK (length(nip) = 10 AND nip GLOB '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+                        nazwa_firmy TEXT NOT NULL,
+                        nr_tel TEXT
+                    );
+                    ";
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Database created and initialized");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KatalogGierKomp
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Image { get; set; } = "";     //bo stringi nie są nullable
+        public string Name { get; set; } = ""; 
+        public int Score { get; set; }
+        public string Review { get; set; } = "";
+        public int Completion { get; set; }
+    }
+}
